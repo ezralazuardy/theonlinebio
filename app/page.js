5cf1ce536cbeca1fcda09811f095d99e3f686934
Ezra Lazuardy <ezralazuardy@icloud.com>
2025-04-03 20:21:49 +0700

    feat: update

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Background from "@/components/ui/background";
 import { Button } from "@/components/ui/button";
 import Header from "@/components/ui/header";
 import InquiryForm from "@/components/ui/inquiry-form";
+import Showcase, { ShowcaseLabel } from "@/components/ui/showcase";
 import Link from "next/link";
 
 export default function Home() {
@@ -15,33 +16,35 @@ export default function Home() {
       </div>
       <main className="absolute z-10 bottom-0 left-0 w-full flex flex-col">
         <div className="flex w-full justify-center">
-          <h1 className="pb-10 text-6xl font-light leading-tight max-w-4xl">
+          <h1 className="text-6xl font-light leading-tight max-w-4xl">
             In these digital era, your online presence matters the most.
           </h1>
         </div>
-        <div className="grid grid-cols-2 w-full min-h-52 border-t border-neutral-600/70">
-          <div className="flex space-x-6 w-full border-r border-neutral-600/70 justify-center items-center backdrop-filter backdrop-blur-md">
-            <div className="aspect-video w-auto h-24 bg-neutral-600 rounded-sm"></div>
-            <div className="aspect-video w-auto h-24 bg-neutral-600 rounded-sm"></div>
-            <div className="aspect-video w-auto h-24 bg-neutral-600 rounded-sm"></div>
+        <div className="relative grid grid-cols-2 w-full">
+          <div className="flex flex-col w-full">
+            <Showcase />
           </div>
-          <div className="flex flex-col w-full py-10 pl-16 pe-20 justify-center backdrop-filter backdrop-blur-md">
-            <p className="text-sm text-justify mb-6">
-              We craft unique and professional online portfolios, bios, and profile websites that
-              help Professionals and Entrepreneurs stand out and connect with their audience.
-            </p>
-            <div className="flex space-x-4">
-              <InquiryForm />
-              <span className="mt-2 text-sm">or</span>
-              <Button variant="secondary" asChild>
-                <Link
-                  href={process.env.NEXT_PUBLIC_APPOINTMENT_LINK}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Talk to Us
-                </Link>
-              </Button>
+          <div className="flex flex-col w-full">
+            <ShowcaseLabel className="invisible" />
+            <div className="min-h-48 flex flex-col w-full py-10 pl-16 pe-20 justify-center backdrop-filter backdrop-blur-md border-t border-neutral-600/70">
+              <p className="text-sm text-justify mb-6">
+                We craft unique and professional online portfolios, bios, and
+                profile websites that help Professionals and Entrepreneurs stand
+                out and connect with their audience.
+              </p>
+              <div className="flex space-x-4">
+                <InquiryForm />
+                <span className="mt-2 text-sm">or</span>
+                <Button variant="secondary" asChild>
+                  <Link
+                    href={process.env.NEXT_PUBLIC_APPOINTMENT_LINK}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Talk to Us
+                  </Link>
+                </Button>
+              </div>
             </div>
           </div>
         </div>
---
+++ components/ui/showcase.js
@@ -0,0 +1,111 @@
+"use client";
+
+import { ArrowDown, ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+
+export default function Showcase() {
+  const containerRef = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    if (!container) return;
+
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+    };
+
+    container.addEventListener("mouseenter", handleMouseEnter);
+    container.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      container.removeEventListener("mouseenter", handleMouseEnter);
+      container.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col w-full">
+      <ShowcaseLabel
+        className={`transition-all duration-500 transform ${isHovered ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"}`}
+      />
+      <div
+        ref={containerRef}
+        className="min-h-48 flex w-full py-10 px-20 space-x-6 justify-center items-center backdrop-filter backdrop-blur-md border-t border-r border-neutral-600/70"
+      >
+        <ShowcaseItems />
+      </div>
+    </div>
+  );
+}
+
+export function ShowcaseItems() {
+  return (
+    <>
+      <Link
+        href="https://ezra.theonline.bio"
+        className="relative aspect-video w-auto h-24 rounded-sm border border-neutral-600/70 group"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="flex w-full h-full bg-neutral-600 rounded-sm animate-pulse"></div>
+        <div className="absolute top-0 left-0 cursor-pointer w-full h-full rounded-sm bg-black/40 transition-opacity duration-500 opacity-0 group-hover:opacity-100">
+          <ArrowUpRight className="size-5 absolute top-2 right-2" />
+          <span className="font-mono text-xs absolute bottom-2 left-3">
+            ezra.
+            <br />
+            theonline.bio
+          </span>
+        </div>
+      </Link>
+      <Link
+        href="https://stephanie.theonline.bio"
+        className="relative aspect-video w-auto h-24 rounded-sm border border-neutral-600/70 group"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="flex w-full h-full bg-neutral-600 rounded-sm animate-pulse"></div>
+        <div className="absolute top-0 left-0 cursor-pointer w-full h-full rounded-sm bg-black/40 transition-opacity duration-500 opacity-0 group-hover:opacity-100">
+          <ArrowUpRight className="size-5 absolute top-2 right-2" />
+          <span className="font-mono text-xs absolute bottom-2 left-3">
+            stephanie.
+            <br />
+            theonline.bio
+          </span>
+        </div>
+      </Link>
+      <Link
+        href="https://bhaska.theonline.bio"
+        className="relative aspect-video w-auto h-24 rounded-sm border border-neutral-600/70 group"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="flex w-full h-full bg-neutral-600 rounded-sm animate-pulse"></div>
+        <div className="absolute top-0 left-0 cursor-pointer w-full h-full rounded-sm bg-black/40 transition-opacity duration-500 opacity-0 group-hover:opacity-100">
+          <ArrowUpRight className="size-5 absolute top-2 right-2" />
+          <span className="font-mono text-xs absolute bottom-2 left-3">
+            bhaska.
+            <br />
+            theonline.bio
+          </span>
+        </div>
+      </Link>
+    </>
+  );
+}
+
+export function ShowcaseLabel({ className }) {
+  return (
+    <div className={`flex w-full ps-20 ${className}`}>
+      <div className="flex px-4 py-2 bg-white/60 text-sm text-black border-t border-l border-r border-neutral-600/70">
+        Latest Showcase <ArrowDown className="size-4 ml-2 mt-0.5" />
+      </div>
+    </div>
+  );
+}
